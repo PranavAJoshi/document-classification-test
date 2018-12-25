a27d4c3b26048206232ea992dea2739ace21fabf
Justin Yang <37907441+xuanyangece@users.noreply.github.com>
2018-12-24 21:46:24 -0500

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -2,6 +2,8 @@
 import pandas
 import matplotlib.pyplot as plt
 from sklearn import model_selection
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 
 # Get dataset from csv file
 names = ['type', 'content']
@@ -10,3 +12,11 @@ dataframe = pandas.read_csv('shuffled-full-set-hashed.csv', names=names)
 # Show the composition of our dataset
 print(dataframe.type.size)
 dataframe.type.value_counts().plot(kind='pie', figsize=(16,16))
+
+# Split dataset
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(dataframe, dataframe.type, test_size=0.20, random_state=0)
+
+# Feature engineering
+vectorizer = CountVectorizer()
+transformer = TfidfTransformer()
+tfidf = transformer.fit_transform(vectorizer.fit_transform(X_train.content.values.astype('U')))
