19d17b401e0db3e9fc22a8061cbb64a0eb047a64
Justin Yang <37907441+xuanyangece@users.noreply.github.com>
2018-12-27 14:02:53 -0500

    Update app.py
    refine for result page

--- flask-directory/app.py
+++ flask-directory/app.py
@@ -10,14 +10,14 @@ vecname = 'vec.pkl'
 def index():
     return flask.render_template('index.html')
     
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/words', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
         data = request.form['content']
         model = pickle.load(open(modelname, 'rb'))
         vec = pickle.load(open(vecname, 'rb'))
-        result = getresult(model, vec, data)
-        return result[0]
+        result = getresult(model, vec, data)[0]
+        return flask.render_template('result.html', result=result)
     
 def getresult(model, vec, data):
     transformed_data = vec.transform([data])
